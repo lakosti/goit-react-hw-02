efc4e59b54f0bd447bda4cce1ddf732f081a7e41
Yaroslava <yaroslava.hovenko@gmail.com>
2024-03-02 20:59:28 +0200

    add new information

--- src/assets/components/Feedback/Feedback.jsx
+++ src/assets/components/Feedback/Feedback.jsx
@@ -2,23 +2,26 @@ import css from "./Feedback.module.css";
 
 const Feedback = () => {
   return (
-    <div className={css.feedbackWrap}>
-      <p className={css.feedback}>
-        Good: <span className={css.feedbackGood}>{}</span>
-      </p>
-      <p className={css.feedback}>
-        Neutral: <span className={css.feedbackNeutral}>{}</span>
-      </p>
-      <p className={css.feedback}>
-        Bad: <span className={css.feedbackBad}>{}</span>
-      </p>
-      <p className={css.feedback}>
-        Total: <span className={css.feedbackText}>{}</span>
-      </p>
-      <p className={css.feedback}>
-        Positive: <span className={css.feedbackText}>{}</span>
-      </p>
-    </div>
+    <>
+      <p className={css.withoutFeedback}>Not feedback yet!</p>
+      <div className={css.feedbackWrap}>
+        <p className={css.feedback}>
+          Good: <span className={css.feedbackGood}>{}</span>
+        </p>
+        <p className={css.feedback}>
+          Neutral: <span className={css.feedbackNeutral}>{}</span>
+        </p>
+        <p className={css.feedback}>
+          Bad: <span className={css.feedbackBad}>{}</span>
+        </p>
+        <p className={css.feedback}>
+          Total: <span className={css.feedbackText}>{}</span>
+        </p>
+        <p className={css.feedback}>
+          Positive: <span className={css.feedbackText}>{}</span>
+        </p>
+      </div>
+    </>
   );
 };
 
