a7b8516ad8aa6b3b9babbb9cfdecbf012d2c8718
Yaroslava <yaroslava.hovenko@gmail.com>
2024-03-02 20:50:59 +0200

    update

--- src/assets/components/Feedback/Feedback.jsx
+++ src/assets/components/Feedback/Feedback.jsx
@@ -4,19 +4,19 @@ const Feedback = () => {
   return (
     <div className={css.feedbackWrap}>
       <p className={css.feedback}>
-        Good: <span className={css.feedbackGood}>{5}</span>
+        Good: <span className={css.feedbackGood}>{}</span>
       </p>
       <p className={css.feedback}>
-        Neutral: <span className={css.feedbackNeutral}>{4}</span>
+        Neutral: <span className={css.feedbackNeutral}>{}</span>
       </p>
       <p className={css.feedback}>
-        Bad: <span className={css.feedbackBad}>{5}</span>
+        Bad: <span className={css.feedbackBad}>{}</span>
       </p>
       <p className={css.feedback}>
-        Total: <span className={css.feedbackText}>{4}</span>
+        Total: <span className={css.feedbackText}>{}</span>
       </p>
       <p className={css.feedback}>
-        Positive: <span className={css.feedbackText}>{5}</span>
+        Positive: <span className={css.feedbackText}>{}</span>
       </p>
     </div>
   );
