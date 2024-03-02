61b2c839f96aa9e6a81e52aaed6e88daf6fe8ded
Yaroslava <yaroslava.hovenko@gmail.com>
2024-03-02 21:21:46 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Description from "./assets/components/Description/Description";
 import Feedback from "./assets/components/Feedback/Feedback";
+import Notification from "./assets/components/Notification/Notification";
 import Options from "./assets/components/Options/Options";
 
 // const feedback = {
@@ -13,7 +14,9 @@ function App() {
     <div className="wrapper">
       <Description />
       <Options />
+      <Notification />
       <Feedback />
+      {/* {<Feedback /> ? <Notification /> : ""} */}
     </div>
   );
 }
--- src/assets/components/Feedback/Feedback.jsx
+++ src/assets/components/Feedback/Feedback.jsx
@@ -2,26 +2,23 @@ import css from "./Feedback.module.css";
 
 const Feedback = () => {
   return (
-    <>
-      <p className={css.withoutFeedback}>Not feedback yet!</p>
-      <div className={css.feedbackWrap}>
-        <p className={css.feedback}>
-          Good: <span className={css.feedbackGood}>{}</span>
-        </p>
-        <p className={css.feedback}>
-          Neutral: <span className={css.feedbackNeutral}>{}</span>
-        </p>
-        <p className={css.feedback}>
-          Bad: <span className={css.feedbackBad}>{}</span>
-        </p>
-        <p className={css.feedback}>
-          Total: <span className={css.feedbackText}>{}</span>
-        </p>
-        <p className={css.feedback}>
-          Positive: <span className={css.feedbackText}>{}</span>
-        </p>
-      </div>
-    </>
+    <div className={css.feedbackWrap}>
+      <p className={css.feedback}>
+        Good: <span className={css.feedbackGood}>{}</span>
+      </p>
+      <p className={css.feedback}>
+        Neutral: <span className={css.feedbackNeutral}>{}</span>
+      </p>
+      <p className={css.feedback}>
+        Bad: <span className={css.feedbackBad}>{}</span>
+      </p>
+      <p className={css.feedback}>
+        Total: <span className={css.feedbackText}>{}</span>
+      </p>
+      <p className={css.feedback}>
+        Positive: <span className={css.feedbackText}>{}</span>
+      </p>
+    </div>
   );
 };
 
