d638b13815a30e9d880325af399000c045d4ed7b
Yaroslava <yaroslava.hovenko@gmail.com>
2024-03-05 21:15:19 +0200

    fix way

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Description from "./assets/components/Description/Description";
-import Feedback from "./assets/components/Feedback/Feedback";
-import Notification from "./assets/components/Notification/Notification";
-import Options from "./assets/components/Options/Options";
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
+import Options from "./components/Options/Options";
 
 function App() {
   const feedback = {
